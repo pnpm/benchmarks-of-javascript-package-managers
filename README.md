7c0d2d2cbd325d42a05f70194ddeb0faa6593349
zkochan <zoltan.kochan@gmail.com>
2016-11-07 04:18:37 +0200

    feat: generate a README from the results

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Node package manager benchmark
+
+This benchmark compares the performance of [npm](https://github.com/npm/npm), [pnpm](https://github.com/rstacruz/pnpm) and [yarn](https://github.com/yarnpkg/yarn).
+
+| command | time in ms |
+| --- | --- |
+| npm install babel-cli | 37261 |
+| yarn add babel-cli | 6886 |
+| pnpm install babel-cli | 5743 |
--- index.js
+++ index.js
@@ -16,14 +16,6 @@ child.spawnSync('npm', ['cache', 'clean'], {env, stdio: 'inherit'})
 child.spawnSync('yarn', ['cache', 'clean'], {env, stdio: 'inherit'})
 child.spawnSync('pnpm', ['cache', 'clean'], {env, stdio: 'inherit'})
 
-console.log('start')
-
-benchmark('npm', ['install', 'babel-cli', '-S', '--force', '--ignore-scripts'])
-
-benchmark('yarn', ['add', 'babel-cli', '--force', '--ignore-scripts'])
-
-benchmark('pnpm', ['install', 'babel-cli', '-S', '--ignore-scripts', '--store-path', '"node_modules/.store"'])
-
 function createEnv () {
   const env = Object.create(process.env)
   env[PATH] = [
@@ -38,12 +30,31 @@ function benchmark (cmd, args) {
   const timeName = `${cmd} ${args.join(' ')}`
   const cwd = path.join(__dirname, '.tmp', cmd)
   mkdirp(cwd)
-  console.time(timeName)
+  const startTime = Date.now()
   fs.writeFileSync(path.join(cwd, 'package.json'), '{"name":"foo","version":"1.0.0"}', 'utf-8')
-  const result = child.spawnSync(cmd, args, {env, cwd})
+  const result = child.spawnSync(cmd, args, {env, cwd, stdio: 'inherit'})
   if (result.status !== 0) {
     throw new Error(`${timeName} failed with status code ${result.status}`)
   }
-  console.timeEnd(timeName)
+  const endTime = Date.now()
   rimraf(cwd)
+  return endTime - startTime
 }
+
+const npmTime = benchmark('npm', ['install', 'babel-cli', '-S', '--force', '--ignore-scripts'])
+
+const yarnTime = benchmark('yarn', ['add', 'babel-cli', '--force', '--ignore-scripts'])
+
+const pnpmTime = benchmark('pnpm', ['install', 'babel-cli', '-S', '--ignore-scripts', '--store-path', '"node_modules/.store"'])
+
+fs.writeFile('README.md', `
+# Node package manager benchmark
+
+This benchmark compares the performance of [npm](https://github.com/npm/npm), [pnpm](https://github.com/rstacruz/pnpm) and [yarn](https://github.com/yarnpkg/yarn).
+
+| command | time in ms |
+| --- | --- |
+| npm install babel-cli | ${npmTime} |
+| yarn add babel-cli | ${yarnTime} |
+| pnpm install babel-cli | ${pnpmTime} |
+`, 'utf8')
