6516ade2bd5cace93fe515cee3618f8aa415021c
BorodinDemid <dev.demid@gmail.com>
2016-12-09 20:13:33 +0200

    change App component

--- site/src/App.js
+++ site/src/App.js
@@ -1,16 +1,9 @@
 import React, {Component} from 'react';
 import BenchmarkResults from './component/BenchmarkResults';
 
-import stats from './stats.json'
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <BenchmarkResults data={ stats }/>
-            </div>
-        );
-    }
-}
-
+const App = ({data}) => {
+    return (
+        <BenchmarkResults data={data}/>
+    );
+};
 export default App;
