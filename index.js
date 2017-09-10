bfb1b57387ca97eda5467afd7f4286407dcd146e
nikoladev <nikola@pilipovic.nl>
2017-09-10 22:14:32 +0200

    use promisified writeFile; refactor some code; add callback to readme writeFile

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
-const fs = require('fs')
+const { promisify } = require('util')
+const writeFile = promisify(require('fs').writeFile)
+const { join } = require('path')
 const stripIndents = require('common-tags').stripIndents
 const prettyMs = require('pretty-ms')
 const benchmark = require('./lib/recordBenchmark')
@@ -22,52 +24,84 @@ const fixtures = [
   }
 ]
 
+const tests = [
+  'firstInstall',
+  'repeatInstall',
+  'withWarmCacheAndLockfile',
+  'withWarmCache',
+  'withLockfile'
+]
+
+const toArray = (pms, resultsObj) => {
+  /**
+   * Make array of all similar installs grouped together:
+   * [
+   *   [ npm.firstInstall, yarn.firstInstall, pnpm.firstInstall ],
+   *   [ npm.repeatInstall, yarn.repeatInstall, pnpm.repeatInstall ],
+   *   ...
+   * ]
+   */
+  return tests
+    .map((test) => pms
+      .map((pm) => resultsObj[pm][test])
+        .map((time) => Math.round(time / 100) / 10) // round to `x.x` seconds
+    )
+}
+
 run()
   .then(() => console.log('done'))
   .catch(err => console.error(err))
 
 async function run () {
+  const pms = [ 'npm', 'yarn', 'pnpm' ]
   const sections = []
+  const svgs = []
   for (const fixture of fixtures) {
-    const npmResults = average(await benchmark('npm', fixture.name, {limitRuns: LIMIT_RUNS}))
-    const yarnResults = average(await benchmark('yarn', fixture.name, {limitRuns: LIMIT_RUNS}))
-    const pnpmResults = average(await benchmark('pnpm', fixture.name, {limitRuns: LIMIT_RUNS}))
-    generateSvg({
-      'npm': npmResults,
-      'yarn': yarnResults,
-      'pnpm': pnpmResults
-    }, fixture.name)
+    const npmRes = average(await benchmark('npm', fixture.name, {limitRuns: LIMIT_RUNS}))
+    const yarnRes = average(await benchmark('yarn', fixture.name, {limitRuns: LIMIT_RUNS}))
+    const pnpmRes = average(await benchmark('pnpm', fixture.name, {limitRuns: LIMIT_RUNS}))
+    const resArray = toArray(pms, {
+      'npm': npmRes,
+      'yarn': yarnRes,
+      'pnpm': pnpmRes
+    })
+
     sections.push(stripIndents`
       ${fixture.mdDesc}
 
       | action  | cache | lockfile | node_modules| npm | Yarn | pnpm |
       | ---     | ---   | ---      | ---         | --- | --- | --- |
-      | install |       |          |             | ${npmResults.firstInstall} | ${yarnResults.firstInstall} | ${pnpmResults.firstInstall} |
-      | install | ✔    | ✔        | ✔           | ${npmResults.repeatInstall} | ${yarnResults.repeatInstall} | ${pnpmResults.repeatInstall} |
-      | install | ✔    | ✔        |             | ${npmResults.withWarmCacheAndLockfile} | ${yarnResults.withWarmCacheAndLockfile} | ${pnpmResults.withWarmCacheAndLockfile} |
-      | install | ✔    |          |             | ${npmResults.withWarmCache} | ${yarnResults.withWarmCache} | ${pnpmResults.withWarmCache} |
-      | install |      | ✔        |             | ${npmResults.withLockfile} | ${yarnResults.withLockfile} | ${pnpmResults.withLockfile} |
+      | install |       |          |             | ${prettyMs(npmRes.firstInstall)} | ${prettyMs(yarnRes.firstInstall)} | ${prettyMs(pnpmRes.firstInstall)} |
+      | install | ✔    | ✔        | ✔           | ${prettyMs(npmRes.repeatInstall)} | ${prettyMs(yarnRes.repeatInstall)} | ${prettyMs(pnpmRes.repeatInstall)} |
+      | install | ✔    | ✔        |             | ${prettyMs(npmRes.withWarmCacheAndLockfile)} | ${prettyMs(yarnRes.withWarmCacheAndLockfile)} | ${prettyMs(pnpmRes.withWarmCacheAndLockfile)} |
+      | install | ✔    |          |             | ${prettyMs(npmRes.withWarmCache)} | ${prettyMs(yarnRes.withWarmCache)} | ${prettyMs(pnpmRes.withWarmCache)} |
+      | install |      | ✔        |             | ${prettyMs(npmRes.withLockfile)} | ${prettyMs(yarnRes.withLockfile)} | ${prettyMs(pnpmRes.withLockfile)} |
 
       ![Graph of the ${fixture.name} results](./results/imgs/${fixture.name}.svg)
     `)
+
+    svgs.push({
+      path: join(__dirname, 'results', 'imgs', `${fixture.name}.svg`),
+      file: generateSvg(resArray, pms)
+    })
   }
 
-  fs.writeFile('README.md', stripIndents`
-    # Node package manager benchmark
+  await Promise.all(
+    svgs.map((file) => writeFile(file.path, file.file, 'utf-8')),
+    writeFile('README.md', stripIndents`
+      # Node package manager benchmark
 
-    This benchmark compares the performance of [npm](https://github.com/npm/npm), [pnpm](https://github.com/pnpm/pnpm) and [yarn](https://github.com/yarnpkg/yarn).
+      This benchmark compares the performance of [npm](https://github.com/npm/npm), [pnpm](https://github.com/pnpm/pnpm) and [yarn](https://github.com/yarnpkg/yarn).
 
-    ${sections.join('\n\n')}`, 'utf8')
+      ${sections.join('\n\n')}`, 'utf8')
+  ).catch((err) => { throw err })
 }
 
 function average (benchmarkResults) {
   const results = {}
-  ;['firstInstall', 'repeatInstall', 'withWarmCacheAndLockfile', 'withWarmCache', 'withLockfile']
-    .forEach(measurmentName => {
-      const avgMs = benchmarkResults.map(res => res[measurmentName]).reduce(sum, 0) / benchmarkResults.length
-      results[measurmentName] = prettyMs(avgMs)
-      results[measurmentName + 'Ms'] = avgMs
-    })
+  tests.forEach(test => {
+    results[test] = benchmarkResults.map(res => res[test]).reduce(sum, 0) / benchmarkResults.length
+  })
   return results
 }
 
